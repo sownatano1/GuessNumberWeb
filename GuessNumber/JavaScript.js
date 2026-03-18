8b24fd707c518f90983ed488cb8022d272b3b560
sownatano1 <161262646+sownatano1@users.noreply.github.com>
2026-03-18 16:15:47 -0300

    2.0

--- GuessNumber/JavaScript.js
+++ GuessNumber/JavaScript.js
@@ -1,6 +1,12 @@
 let randoness = parseInt(Math.random() * 100 + 1 )
 let correctNumber = randoness
 
+const paragraph = document.createElement("p")
+paragraph.classList.add("hint")
+const image = document.createElement("img")
+image.classList.add("winPNG")
+const container = document.getElementById("container")
+
 function Input()
 {
     let inputNum = document.getElementById("numberInput")
@@ -11,39 +17,69 @@ function Input()
     
     if (number == correctNumber)
     {
-        document.getElementById("hint").textContent = "Acertou!"
-        document.getElementById("image").src = "WinPNG.png"
+        paragraph.textContent = "Acertou!";
+        container.appendChild(paragraph)
+        RandomImage()
         setTimeout(() => 
         {
             Reset()
         }, 2000);
     }
 
-    else if (number > number + 10 || number < number - 10)
+    else if (correctNumber - 3 < number && number < correctNumber + 3)
     {
-        console.log("Numero esta proximo")
+        paragraph.textContent = "O número está próximo";
+        container.appendChild(paragraph)
     }
 
     else if (number == 0)
     {   
-        document.getElementById("hint").textContent = " "
+        paragraph.remove()
     }
 
     else if (number > correctNumber)
     {
-        document.getElementById("hint").textContent = "O numero é menor!"
+        paragraph.textContent = "O número é menor";
+        container.appendChild(paragraph)
     }
 
     else if (number < correctNumber)
     {
-        document.getElementById("hint").textContent = "O numero é maior!"
+        paragraph.textContent = "O número é maior";
+        container.appendChild(paragraph)
     }
 }
 
 function Reset()
 {
-    document.getElementById("hint").textContent = " "
-    document.getElementById("image").src = " "
+    paragraph.remove()
+    image.remove()
     randoness = parseInt(Math.random() * 100 + 1 )
     correctNumber = randoness;
+}
+
+function RandomImage()
+{
+    let randomImg = parseInt(Math.random() * 8)
+    console.log("Numero da image: " + randomImg)
+
+    if (randomImg <= 2)
+    {
+        console.log("cat")
+        image.src = "cat-meme.gif"
+    }
+
+    else if (randomImg <= 5)
+    {
+        console.log("happyCat")
+        image.src = "HappyCat.gif"
+    }
+
+    else if (randomImg <= 8)
+    {
+        console.log("frog")
+        image.src = "froggy.gif"
+    }
+
+    container.appendChild(image)
 }
