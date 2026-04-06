39ef9324da02acad2cd7ebb3faf1a20792a83c07
sownatano1 <161262646+sownatano1@users.noreply.github.com>
2026-04-06 14:31:45 -0300

    q

--- GuessNumber/Aim.js
+++ GuessNumber/Aim.js
@@ -1,5 +1,6 @@
-canva = document.getElementById("canva")
-height = 400
-width = 400
-area = height * width
-
+const element = document.getElementById('canvas');
+element.addEventListener('click', (event) => {
+  const x = event.clientX;
+  const y = event.clientY;
+  console.log(`A posição atual do cursor do mouse é: (${x}, ${y})`);
+});
